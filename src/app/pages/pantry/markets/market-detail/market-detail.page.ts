287a02cae85bbee7831127d2f0ffb2cf948986f3
darthleonard <leolinuxmx@gmail.com>
2020-10-09 23:56:55 -0500

    market page functionality added

--- src/app/pages/pantry/markets/market-detail/market-detail.page.ts
+++ src/app/pages/pantry/markets/market-detail/market-detail.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Storage } from '@ionic/storage';
+import { Location } from '@angular/common';
+import { BaseDetailPage } from 'src/app/pages/base/base-detail.page';
+import { MarketRecord } from 'src/app/models/market-record.model';
 
 @Component({
   selector: 'app-market-detail',
   templateUrl: './market-detail.page.html',
   styleUrls: ['./market-detail.page.scss'],
 })
-export class MarketDetailPage implements OnInit {
+export class MarketDetailPage  extends BaseDetailPage {
 
-  constructor() { }
+  constructor(public activatedRoute: ActivatedRoute,
+              public storage: Storage,
+              public location: Location)
+  {
+    super(activatedRoute, storage, location);
+  }
 
-  ngOnInit() {
+  recordInit() {
+    this.record = new MarketRecord();
   }
 
 }
